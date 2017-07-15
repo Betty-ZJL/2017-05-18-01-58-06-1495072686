9e6c273484374dd3dcbb7da54079eea40dacd3ee
Betty-ZJL <Betty-ZJL@users.noreply.github.com>
2017-07-15 15:35:33 +0800

    Update main.js

--- main/main.js
+++ main/main.js
@@ -1,3 +1,6 @@
-module.exports = function main() {
-  // Write your cade here
+module.exports = function main(str) {
+  var sum=0;
+  for (var i=0;i<str.length;i++)
+      sum=sum+parseInt(str.charAt(i));
+  return sum;
 };
